aa573ddde747511eca771772203dec3db93e6fad
Amy Jung <ajwjung@outlook.com>
2025-03-21 16:37:37 -0700

    style: Style cards at larger breakpoints

--- client/src/components/AllDrinks.jsx
+++ client/src/components/AllDrinks.jsx
@@ -116,7 +116,7 @@ function AllDrinks() {
                       : category.drinkType
                     }
                   </h2>
-                  <div className="row row-cols-2 row-cols-md-3 g-4" >
+                  <div className="row row-cols-2 row-cols-md-3 row-cols-lg-4 g-4" >
                     {category.drinks.map((drinkGroup, i) => {
                       return (
                         <div className="col" key={i}>
