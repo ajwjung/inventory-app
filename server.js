a5c2e71645403128839d431dc44ebf90044371fb
Amy Jung <ajwjung@outlook.com>
2025-03-22 16:42:51 -0700

    chore: Update build script to try deployment again

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors({ origin: "*",
   methods: ["GET", "PUT", "POST", "DELETE"] })
 ); // Allow requests from any origin (can be more specific if needed)
@@ -18,11 +18,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", drinksRouter);
 
 // Serve static files from the React app's dist directory
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(path.join(__dirname, "client", "dist")));
 
 // All other requests should be directed to the React app's index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
