37d5c54f519e8ec4f4920d379c5aa8fb3eeccda0
Amy Jung <ajwjung@outlook.com>
2025-03-21 21:46:43 -0700

    fix: React app not rendering with backend

--- routes/drinksRouter.js
+++ routes/drinksRouter.js
@@ -3,19 +3,16 @@ const drinksController = require("../controllers/drinksController.js");
 
 const drinksRouter = Router();
 
-drinksRouter.get("/", (req, res) => {
-  res.send("Hello, World!");
-})
+drinksRouter.get("/all-items", drinksController.getDrinksWithInfo);
+drinksRouter.put("/all-items", drinksController.editDrink);
+drinksRouter.post("/all-items", drinksController.addNewDrink);
+drinksRouter.delete("/all-items/:drinkId", drinksController.deleteDrink);
 
-drinksRouter.get("/api/all-items", drinksController.getDrinksWithInfo);
-drinksRouter.put("/api/all-items", drinksController.editDrink);
-drinksRouter.post("/api/all-items", drinksController.addNewDrink);
-drinksRouter.delete("/api/all-items/:drinkId", drinksController.deleteDrink);
+drinksRouter.get("/all-categories", drinksController.getAllDrinkTypes);
+drinksRouter.post("/all-categories", drinksController.addNewDrinkType);
+drinksRouter.put("/all-categories", drinksController.editDrinkType);
+drinksRouter.delete("/all-categories/:categoryId", drinksController.deleteDrinkType);
 
-drinksRouter.get("/api/all-categories", drinksController.getAllDrinkTypes);
-drinksRouter.post("/api/all-categories", drinksController.addNewDrinkType);
-drinksRouter.put("/api/all-categories", drinksController.editDrinkType);
-drinksRouter.delete("/api/all-categories/:categoryId", drinksController.deleteDrinkType);
-drinksRouter.get("/api/all-milk-substitutes", drinksController.getAllMilkSubstitutes);
+drinksRouter.get("/all-milk-substitutes", drinksController.getAllMilkSubstitutes);
 
 module.exports = drinksRouter;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require("path");
 
 const express = require("express");
 const drinksRouter = require("./routes/drinksRouter");
@@ -14,6 +15,14 @@ app.use(cors({ origin: "*",
 app.use(express.json());  // Parse JSON requests (from forms) and put into req.body
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", drinksRouter);
+app.use("/api", drinksRouter);
+
+// Serve static files from the React app's dist directory
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+// All other requests should be directed to the React app's index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
